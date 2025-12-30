974cac47b1dc9883bea5b32e5dd99027e3415473
cyborgcomputercarangki-bit <cyborgcomputercarangki@gmail.com>
2025-12-31 01:16:13 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+// sw.js
+const CACHE_NAME = 'macca-v1';
+const urlsToCache = ['driver.html', 'manifest.json'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
